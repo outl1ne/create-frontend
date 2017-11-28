e5700eead78db07b8ab1eb756dafb4235b7c3d72
Matis Lepik <matis.lepik@gmail.com>
2017-11-28 22:18:49 +0200

    Update package.json generating logic
    We now create the object using plain javascript, and stringify later, instead of creating it in string form to begin with. This should help catch any bugs before they make it into the destination

--- scripts/getPackageJson.js
+++ scripts/getPackageJson.js
@@ -1,18 +1,21 @@
 'use strict';
 const path = require('path');
 
-module.exports = ({ name, isDev }) => `{
-  "name": "${name}",
-  "version": "0.1.0",
-  "private": true,
-  "scripts": {
-    "dev": "od-frontend dev",
-    "build": "od-frontend build",
-    "build:debug": "od-frontend build --debug"
-  },
-  "dependencies": {
-    "normalize.css": "7.x.x",
-    "od-frontend": "${isDev ? path.resolve(__dirname, '..') : '1.x.x'}"
-  }
-}
-`;
+module.exports = ({ name, isDev }) => {
+  const json = {
+    name: name,
+    version: '0.1.0',
+    private: true,
+    scripts: {
+      dev: 'od-frontend dev',
+      build: 'od-frontend build',
+      'build:debug': 'od-frontend build --debug',
+    },
+    dependencies: {
+      'normalize.css': '7.x.x',
+      'od-frontend': isDev ? path.resolve(__dirname, '..') : '1.x.x',
+    },
+  };
+
+  return JSON.stringify(json, null, 2);
+};

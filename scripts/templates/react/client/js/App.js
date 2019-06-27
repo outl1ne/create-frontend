59e37e3527d6fb742f5e6a1db2f3b807a1092b24
Matis Lepik <matis.lepik@gmail.com>
2019-06-27 10:27:36 +0300

    Change react template App.js

--- scripts/templates/react/client/js/App.js
+++ scripts/templates/react/client/js/App.js
@@ -1,12 +1,5 @@
-/* @flow */
 import React from 'react';
 
-export type Props = {};
-
-export default class App extends React.Component {
-  props: Props;
-
-  render() {
-    return <div>Hello world!</div>;
-  }
+export default function App() {
+  return <div>Hello world!</div>;
 }

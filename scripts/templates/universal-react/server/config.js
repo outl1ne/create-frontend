1d2970b3a987d6318107e7ae625492ea85c459a4
Matis Lepik <matis.lepik@gmail.com>
2020-02-28 09:17:01 +0200

    Edit config documentation to be less verbose

--- scripts/templates/universal-react/server/config.js
+++ scripts/templates/universal-react/server/config.js
@@ -1,7 +1,6 @@
 /**
- * This is the app config file. It's defined in the server so you can access process.env.
- * It will be exposed to the client-side as well. In React, AppDataContext can be
- * used to gain access to this configuration in both the client and server.
+ * This is the app config file. It's defined in the server, so you can access process.env.
+ * In React, AppDataContext can be used to access this config in both the client and server.
  */
 module.exports = () => ({
   APP_NAME: 'New app',

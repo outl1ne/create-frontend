8f389130f2ad57d90129940a04fdf6a1898fc33e
Matis Lepik <matis.lepik@gmail.com>
2021-04-13 13:02:45 +0300

    Move helmet default options declaration to middleware fn

--- universal-react/helmet.js
+++ universal-react/helmet.js
@@ -1,46 +1,46 @@
 import helmet from 'helmet';
 import crypto from 'crypto';
 
-export const defaultOpts = {
-  hsts: {
-    includeSubDomains: false,
-  },
-  contentSecurityPolicy: {
-    directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      'font-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', 'fonts.gstatic.com'].filter(Boolean),
-      'img-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', '*', 'data:'].filter(Boolean),
-      'media-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', '*', 'data:'].filter(Boolean),
-      'default-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', __DEVELOPMENT__ && 'ws://localhost:*'].filter(
-        Boolean
-      ),
-      'script-src': [
-        "'self'",
-        __DEVELOPMENT__ && 'localhost:*',
-        (_, res) => `'nonce-${res.locals.cspNonce}'`,
-        'www.google-analytics.com',
-        'ajax.googleapis.com',
-        'https://www.googletagmanager.com',
-      ].filter(Boolean),
-      'style-src': ["'self'", "https: 'unsafe-inline'", 'fonts.googleapis.com'],
-      'connect-src': [
-        "'self'",
-        __DEVELOPMENT__ && 'localhost:*',
-        __DEVELOPMENT__ && 'ws://localhost:*',
-        'https://www.google-analytics.com',
-      ].filter(Boolean),
-    },
-  },
-};
-
 /**
  * Wrapper around helmet that provides defaults that work in local development, and generates a nonce for CSP
  * https://helmetjs.github.io/
  *
  * @param {object | function} opts - Options that get passed to helmet. If a function is passed, it receives the default options as an argument, and should return the options object.
  */
-export default function getHelmetMiddleware(opts = defaultOpts) {
-  const helmetMiddleware = helmet(typeof opts === 'function' ? opts({ ...defaultOpts }) : opts);
+export default function getHelmetMiddleware(opts) {
+  const defaultOpts = {
+    hsts: {
+      includeSubDomains: false,
+    },
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        'font-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', 'fonts.gstatic.com'].filter(Boolean),
+        'img-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', '*', 'data:'].filter(Boolean),
+        'media-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', '*', 'data:'].filter(Boolean),
+        'default-src': ["'self'", __DEVELOPMENT__ && 'localhost:*', __DEVELOPMENT__ && 'ws://localhost:*'].filter(
+          Boolean
+        ),
+        'script-src': [
+          "'self'",
+          __DEVELOPMENT__ && 'localhost:*',
+          (_, res) => `'nonce-${res.locals.cspNonce}'`,
+          'www.google-analytics.com',
+          'ajax.googleapis.com',
+          'https://www.googletagmanager.com',
+        ].filter(Boolean),
+        'style-src': ["'self'", "https: 'unsafe-inline'", 'fonts.googleapis.com'],
+        'connect-src': [
+          "'self'",
+          __DEVELOPMENT__ && 'localhost:*',
+          __DEVELOPMENT__ && 'ws://localhost:*',
+          'https://www.google-analytics.com',
+        ].filter(Boolean),
+      },
+    },
+  };
+
+  const helmetMiddleware = helmet(typeof opts === 'function' ? opts(defaultOpts) : opts || defaultOpts);
 
   return (req, res, next) => {
     if (!res.locals.cspNonce) {

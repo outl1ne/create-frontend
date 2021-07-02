cb5110ae7b5f3c5f2a142dbe136a544bec76dda8
Matis Lepik <matis.lepik@gmail.com>
2021-07-02 14:37:47 +0300

    Fix manifest loading issue in dev mode
    Rarely, there would be an error like "unexpected end of JSON input". Seems like a race condition where the server tried to restart before the frontend build had finished writing the manifest file. The new logic will read the manifest during the first request, instead of during startup, and adds better error handling.

--- universal-react/server/wrapInDocument.js
+++ universal-react/server/wrapInDocument.js
@@ -1,8 +1,29 @@
 import fs from 'fs';
 import serialize from 'serialize-javascript';
 
-/* Read manifest using fs, because require() would try to resolve at build-time  */
-const manifest = JSON.parse(fs.readFileSync(__OCF_MANIFEST_PATH__, 'utf8'));
+let _manifest;
+function getManifest() {
+  // If manifest has already been read, reuse it, so we don't need to keep making fs calls in production
+  if (_manifest) return _manifest;
+
+  // Reads manifest using fs, because require() would try to resolve at build-time
+  const manifestString = fs.readFileSync(__OCF_MANIFEST_PATH__, 'utf8');
+
+  try {
+    _manifest = JSON.parse(manifestString);
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Failed to parse manifest. ${
+        __DEVELOPMENT__
+          ? 'It may still be rebuilding. Try again in a second, or restart the dev server.'
+          : `Ensure that the build correctly created a JSON file at ${__OCF_MANIFEST_PATH__}.`
+      }`
+    );
+  }
+
+  return _manifest;
+}
 
 export default function wrapInDocument(content, appData, helmetContext, cspNonce, inlineStyles = []) {
   /* Get dev-only styles, to prevent FOUC. This is a virtual file injected by the dev server. */
@@ -10,6 +31,8 @@ export default function wrapInDocument(content, appData, helmetContext, cspNonce
 
   /* Get page meta data from react-helmet */
   const { helmet } = helmetContext;
+
+  const manifest = getManifest();
 
   return `<!doctype html>
 <html ${helmet.htmlAttributes.toString()}>
